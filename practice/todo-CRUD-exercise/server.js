4588414fa3b1422ad004acd0803d5d03e0971da2
siriusscaper <siriusscaper@gmail.com>
2022-08-17 16:42:04 -0700

    installed node modules and started separating the config information into a separate directory and file

--- practice/todo-CRUD-exercise/server.js
+++ practice/todo-CRUD-exercise/server.js
@@ -6,10 +6,12 @@
 //Declare Variables
 const express = require("express");
 const app = express();
-const PORT = 8000;
 const mongoose = require("mongoose");
 const TodoTask = require("./models/TodoTask");
-require('dotenv').config()
+const dotenv = require('dotenv')
+
+
+dotenv.config({path: './config/config.env'})
 
 //Set Middleware
 app.set("view engine", "ejs");
@@ -21,70 +23,71 @@ mongoose.connect(
     process.env.DB_CONNECTION, 
     { useNewUrlParser: true }, 
     () => {console.log("Connected to db!");}
-)
-
-// GET METHOD
-app.get("/", async (req, res) => {
-    try {
-        TodoTask.find({}, (err, tasks) => {
-            res.render("index.ejs", { todoTasks: tasks });
-        });
-    } catch (err) {
-        if (err) return res.status(500).send(err);
-    }
-});
-
-//POST METHOD
-app.post('/', async (req, res) => {
-    const todoTask = new TodoTask(
-        {
-            title: req.body.title,
-            content: req.body.content
-        });
-    try {
-        await todoTask.save();
-        console.log(todoTask)
-        res.redirect("/");
-    } catch (err) {
-        if (err) return res.status(500).send(err);
-        res.redirect("/");
-    }
-});
-
-//UPDATE METHOD
-app
-    .route("/edit/:id")
-    .get((req, res) => {
-        const id = req.params.id;
-        TodoTask.find({}, (err, tasks) => {
-            res.render("edit.ejs", { todoTasks: tasks, idTask: id });
-        });
-    })
-    .post((req, res) => {
-        const id = req.params.id;
-        TodoTask.findByIdAndUpdate(
-            id,
+    )
+    
+    // GET METHOD
+    app.get("/", async (req, res) => {
+        try {
+            TodoTask.find({}, (err, tasks) => {
+                res.render("index.ejs", { todoTasks: tasks });
+            });
+        } catch (err) {
+            if (err) return res.status(500).send(err);
+        }
+    });
+    
+    //POST METHOD
+    app.post('/', async (req, res) => {
+        const todoTask = new TodoTask(
             {
                 title: req.body.title,
                 content: req.body.content
-            },
-
-            err => {
+            });
+            try {
+                await todoTask.save();
+                console.log(todoTask)
+                res.redirect("/");
+            } catch (err) {
                 if (err) return res.status(500).send(err);
                 res.redirect("/");
-            });
-    });
-
-//DELETE
-app
-    .route("/remove/:id")
-    .get((req, res) => {
-        const id = req.params.id;
-        TodoTask.findByIdAndRemove(id, err => {
-            if (err) return res.send(500, err);
-            res.redirect("/");
+            }
         });
-    });
-
-//Start Server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+        
+        //UPDATE METHOD
+        app
+        .route("/edit/:id")
+        .get((req, res) => {
+            const id = req.params.id;
+            TodoTask.find({}, (err, tasks) => {
+                res.render("edit.ejs", { todoTasks: tasks, idTask: id });
+            });
+        })
+        .post((req, res) => {
+            const id = req.params.id;
+            TodoTask.findByIdAndUpdate(
+                id,
+                {
+                    title: req.body.title,
+                    content: req.body.content
+                },
+                
+                err => {
+                    if (err) return res.status(500).send(err);
+                    res.redirect("/");
+                });
+            });
+            
+            //DELETE
+            app
+            .route("/remove/:id")
+            .get((req, res) => {
+                const id = req.params.id;
+                TodoTask.findByIdAndRemove(id, err => {
+                    if (err) return res.send(500, err);
+                    res.redirect("/");
+                });
+            });
+            
+            //Start Server
+            const PORT = process.env.PORT || 8000
+            app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
